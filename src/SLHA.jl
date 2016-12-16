52b7ef3014d26794afad0f853bed8574b7c910ea
Sujeet Akula <sujeet@freeboson.org>
2016-12-16 13:29:22 +1100

    update

--- src/SLHA.jl
+++ src/SLHA.jl
@@ -3,4 +3,13 @@ abstract SLHA
 abstract SLHABlock
 
 type SLHASimpleBlock{Rank} <: SLHABlock
+  scale::Float64
   block::Array{Float64,Rank}
+end
+
+type SLHAOptionsBlock <: SLHABlock
+  block::Dict{Int64, String}
+end
+
+type SLHASparseBlock <: SLHABlock
+  block::Sparse
